12bed3b9c08ad981446523c195f85c20d864ab1c
svasquez <santivasquez1@gmail.com>
2020-03-04 16:32:50 -0500

    Inicio cortante bidireccional

--- DisenioZapata_V1/Model/CortanteBiridireccional.cs
+++ DisenioZapata_V1/Model/CortanteBiridireccional.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DisenioZapata_V1.Model
 {
-  public  class CortanteBiridireccional:ICalculo
+    public class CortanteBiridireccional : ICalculo
     {
         private Zapata zapata;
 
@@ -15,15 +16,34 @@ namespace DisenioZapata_V1.Model
             get { return zapata; }
             set { zapata = value; }
         }
-
+        public ObservableCollection<float> Vu{get;set;}
+        public float Limite1 { get; set; }
+        public float Limite2 { get; set; }
+        public float SigmaCortante { get; set; }
         public void Calculo_Clase()
         {
-            throw new NotImplementedException();
+            Vu = new ObservableCollection<float>();
+            Dimensionamiento dimensionamiento = Zapata.ReturnDimensionamiento();
+
+            for(int i = 0; i < dimensionamiento.QmaxX.Count; i++)
+            {
+                var Qmax = new float[] { dimensionamiento.QmaxX[i], dimensionamiento.QmaxY[i], dimensionamiento.QminX[i], dimensionamiento.QminY[i] }.Max();
+                Vu.Add(CalcVu(Qmax));
+            }
         }
 
         public void Chequeos_Clase()
         {
             throw new NotImplementedException();
+        }
+        private float CalcVu(float Qmax)
+        {
+            float a, b, c, Vu;
+            a = zapata.L1 * zapata.L2;
+            b = (Zapata.LcX + Zapata.H - Zapata.R);
+            c = (Zapata.LcY + Zapata.H - Zapata.R);
+            Vu = Qmax * 1.4f * (a - (b * c));
+            return Vu;
         }
     }
 }
